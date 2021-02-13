de83484cb1fa4b1692453d8d53e43ecd5c2787ee
yangdehong <405104787@qq.com>
2021-02-13 19:28:32 +0800

    rocketmq api

---
+++ self-rocket/src/main/java/com/ydh/redsheep/selfrocket/SelfRocketApplication.java
@@ -0,0 +1,13 @@
+package com.ydh.redsheep.selfrocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfRocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfRocketApplication.class, args);
+    }
+
+}
---
+++ self-rocket/src/test/java/com/ydh/redsheep/selfrocket/SelfRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydh.redsheep.selfrocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfRocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
